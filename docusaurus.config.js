8047cc8d8e74f39b46ae718977bc2f8ecc90aefc
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2023-10-17 13:34:19 +0530

    remove blog item

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,17 +117,13 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/mindfiredigital/react-canvas-editor.git'
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Canvas Editor, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
